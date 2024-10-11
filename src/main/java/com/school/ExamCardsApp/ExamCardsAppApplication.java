041a2c6a3858507895c6a8ba8bc5d4193210331b
Denis Siliukov <abhem21rus@mail.ru>
2024-10-11 13:07:18 +0300

    Init

---
+++ src/main/java/com/school/ExamCardsApp/ExamCardsAppApplication.java
@@ -0,0 +1,13 @@
+package com.school.ExamCardsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamCardsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamCardsAppApplication.class, args);
+	}
+
+}
